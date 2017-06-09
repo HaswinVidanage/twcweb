fabb3e8c34fc29b3588076a0d1c75ce6dd1a6686
Haswin Vidanage <Haswin Vidanage>
2017-06-09 14:54:50 +0530

    change css home page

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -12,8 +12,9 @@ var Home = React.createClass({
 							fitToSection:false,
 
 							'afterLoad': function(anchorLink, index){
-								console.log("Width : " + $( window ).width());
-								console.log("Height : " + $( window ).height());
+								// console.log("Width/2 : " + $( window ).width()/2);
+								// console.log("Height/2 : " + $( window ).height()/2);
+								//$('#obj-nexus').width( ($( window ).height()) - 2);
 								// if(index == 1){
 								// 	// $('#iphone3, #iphone2, #iphone4').addClass('active');
 								// 	console.log('Index 1');
@@ -40,63 +41,6 @@ var Home = React.createClass({
 	},
 	render: function() {
 		return (
-			// <div id="fullpage">
-      //   <div className="header header-home section" id="section0">
-      //     <div className="header-container">
-      //       <div className="row align-middle align-justify">
-      //         <div className="presentation small-10 medium-6 large-6 align-center">
-      //           <div className="column">
-      //             <div className="hashtags small-12 medium-12 large-11">
-      //               <h1>
-			//
-      //                 <strong>#Creativity-Ahead</strong>
-      //               </h1>
-      //             </div>
-      //             <div className="title small-12 medium-12 large-11">
-      //               <h2>One place for all your Websites & Apps</h2>
-      //             </div>
-			//
-			//
-      //             <div className="logos small-12">
-			//
-      //               <img src="https://prismicio.cdn.prismic.io/prismicio%2Fd590a9ff-fad3-4e71-9453-fca04fa5a66e_logo_header.svg" alt="" />
-			//
-      //             </div>
-      //           </div>
-      //         </div>
-			//
-      //         <div className="illustration small-11 medium-6 large-6 align-right text-center">
-      //           <div className="illu illu-editor">
-			//
-      //             <object data="./images/Home/nexus.svg" type="image/svg+xml">
-      //               <img src="./images/Home/nexus.png" />
-      //             </object>
-			//
-			//
-      //           </div>
-			//
-      //           <div className="illu illu-code text-right">
-			//
-			// 						<object data="./images/Home/browser-window.svg" type="image/svg+xml">
-      //               <img src="./images/Home/browser-window.png" />
-      //             </object>
-      //           </div>
-			//
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-			// 	<div class="section" id="section2">
-			// 		<div class=" ">
-			// 			<h1>Enjoy it</h1>
-			// 		</div>
-			// 	</div>
-			// 	<div class="section" id="section2">
-			// 		<div class=" ">
-			// 			<h1>Enjoy it</h1>
-			// 		</div>
-			// 	</div>
-			// </div>
 
 			<div id="fullpage" className="small-12 medium-12 large-12">
 				<div className="section section0-home small-12 medium-12 large-12" id="section0-home">
@@ -140,9 +84,11 @@ var Home = React.createClass({
 	          </div>
 	        </div>
 				</div>
-				<div className="section small-12 medium-12 large-12" id="section1-home">
-					<div className="section2-home">
-						<h1>Enjoy it</h1>
+				<div className="section small-12 medium-12 large-12 developer-productivity" id="section1-home">
+					<div className="developer-productivity-container row align-justify align-middle">
+						<div className = 'small-12 medium-6 large-5 content column'>
+							<h1>Enjoy it</h1>
+						</div>
 					</div>
 				</div>
 				<div className="section small-12 medium-12 large-12" id="section3-home">
