df07230c85e5a67f0746b9d6f3be197c6b6fcf01
HaswinVidanage <hashdevan@gmail.com>
2017-06-29 17:59:40 +0530

    add svg image support. TODO : convert svg to react components passing url dyn

--- app/components/portfolio/SingleProject.jsx
+++ app/components/portfolio/SingleProject.jsx
@@ -1,12 +1,26 @@
 var React = require('react');
-
-
+var Isvg = require('react-inlinesvg');
+// <img className="project-desktop-thumb" src={thumbnailDesktop.url} alt={title} />
+// <img className="project-tablet-thumb" src={thumbnailTablet.url} alt={title} />
+// <img className="project-mobile-thumb" src={thumbnailMobile.url} alt={title} />
 
 var SingleProject = React.createClass({
+  getInitialState: function () {
+    return { deviceVisible: 'lap' };
+  },
 
+  handleLapClick : function(){
+     this.setState({deviceVisible: 'lap'});
+  },
+  handleTabClick : function(){
+    this.setState({deviceVisible: 'tab'});
+  },
+  handleMobClick : function(){
+    this.setState({deviceVisible: 'mob'});
+  },
   render : function(){
     var {_id, title, description, url, thumbnailDesktop, thumbnailTablet, thumbnailMobile} = this.props;
-
+    var {deviceVisible} = this.state;
     return (
       <div className="single-project-wrap">
         <div>
@@ -15,9 +29,34 @@ var SingleProject = React.createClass({
           <p>{url}</p>
           <a href={url}> Click Here </a>
           <div>
-          <img className="project-desktop-thumb" src={thumbnailDesktop.url} alt={title} />
-          <img className="project-tablet-thumb" src={thumbnailTablet.url} alt={title} />
-          <img className="project-mobile-thumb" src={thumbnailMobile.url} alt={title} />
+
+              {(function() {
+                switch(deviceVisible) {
+                    case 'lap':
+
+                        return (
+                          // <object id={_id} data="./images/portfolio/laptop.svg" type="image/svg+xml" ref="laptop-svg" >
+    	                    //  <img className="project-desktop-thumb" src='./images/portfolio/laptop.png' alt={title} />
+    	                    // </object>
+                          <Isvg src="./images/portfolio/laptop.svg">
+                            <img src="./images/portfolio/laptop.png" />
+                          </Isvg>
+
+                        );
+                    case 'tab':
+                          return <img className="project-tablet-thumb" src='./images/portfolio/tab.svg' alt={title} />;
+                    case 'mob':
+                          return <img className="project-mobile-thumb" src='./images/portfolio/phone.svg' alt={title} />
+                    default:
+                        return null;
+                }
+            })()}
+
+          </div>
+          <div className="row">
+            <button className="button btn-round-red" ref="btn-lap" onClick={this.handleLapClick}> laptop </button>
+            <button className="button btn-round-red" ref="btn-tab" onClick={this.handleTabClick}> Tab </button>
+            <button className="button btn-round-red" ref="btn-mob" onClick={this.handleMobClick}> Mobile </button>
           </div>
         </div>
       </div>
