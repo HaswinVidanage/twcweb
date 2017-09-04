aebbddfda5eb2428185e2ec6c0cea80e2886a5cd
HaswinVidanage <hashdevan@gmail.com>
2017-09-04 22:22:55 +0530

    fix team grid

--- app/components/common/ContactUsBar.jsx
+++ app/components/common/ContactUsBar.jsx
@@ -16,10 +16,14 @@ var ContactUsBar = React.createClass({
           </div>
           <div className="small-8 medium-6 large-6  columns contact-us-btns">
             <div className="small-12 medium-6 large-6 columns">
-              <button className="button btn-round-red-white"><big className="contact-btn-txt">Call Us</big></button>
+              <button className="button btn-round-red-white"><big className="contact-btn-txt">
+                <a href="tel:+13174562564">Call Us</a>
+              </big></button>
             </div>
             <div className="small-12 medium-6 large-6 columns">
-              <button className="button btn-round-red-red"><big className="contact-btn-txt">Contact Us</big></button>
+              <button className="button btn-round-red-red"><big className="contact-btn-txt">
+                <Link to="/contact" className="font-white">Contact Us</Link>
+              </big></button>
             </div>
           </div>
         </div>
--- app/components/home/MiniContactForm.jsx
+++ app/components/home/MiniContactForm.jsx
@@ -1,32 +1,131 @@
 var React = require('react');
 import MapBox from '../common/Map';
-
-
 var {TiMessageTyping} = require('react-icons/lib/ti/');
-var {FaEnvelope} = require('react-icons/lib/fa/');
+var {FaEnvelope, FaSpinner, FaCheck} = require('react-icons/lib/fa/');
+var enquiryAPI = require('enquiryAPI');
+
 var MiniContactForm = React.createClass({
 
+  getInitialState : function() {
+    return {
+      optionsState : 0,
+      submitBtnState: 0
+
+    };
+  },
+  onSubmit:function(e) {
+    e.preventDefault();
+
+    var selectedVal = this.refs.drpPurpose.value;
+    var selectedText = "";
+
+    switch(selectedVal){
+      case '0':
+        selectedText = 'Demo-Request';
+        break;
+      case '1':
+        selectedText = 'Sales-Enquiry';
+        break;
+      case '2':
+        selectedText = 'Customer-Support';
+        break;
+      case '3':
+        selectedText = 'Other';
+        break;
+      default:
+        selectedText = 'error';
+    }
+
+    console.log("selectedText", selectedText);
+    var res = {
+      name: this.refs.name.value,
+      email: this.refs.email.value,
+      phone: this.refs.phone.value,
+      enquiryType : selectedText,
+      message : this.refs.message.value
+    };
+    this.handleContactForm(res);
+  },
+
+  handleSelect: function(event) {
+     this.setState({optionsState: event.target.value});
+     this.refs.drpPurpose.value = event.target.value;
+   },
+
+   handleContactForm:function(res){
+     this.setState({
+       submitBtnState: 1
+     });
+     this.postEnquiry(res);
+   },
+
+   postEnquiry:function(res){
+     var that = this;
+     enquiryAPI.postEnquiry(res).then(function(){
+       that.setState({
+         submitBtnState: 2
+       });
+     },function(e){
+       that.setState({
+         submitBtnState: -1
+       });
+       console.log(e);
+     });
+
+   },
   render:function(){
+    var submitBtnState = this.state.submitBtnState;
+
+    function renderSubmitButton() {
+      console.log('renderSubmitButton', submitBtnState);
+      if(submitBtnState == 0){
+        return (
+          <big><FaEnvelope/>&nbsp;Send</big>
+        );
+      }
+      else if (submitBtnState == 1) {
+        return (
+          <big><FaSpinner/>&nbsp;...</big>
+        );
+      } else if (submitBtnState == 2) {
+        return (
+          <big><FaCheck/>&nbsp;Sent</big>
+        );
+      } else {
+        return (
+          <big><FaEnvelope/>&nbsp;Error</big>
+        );
+      }
+    }
+
     return (
       <div className="MiniContactForm small-12 medium-12 large-12 row" id="MiniContactForm">
           <div className="mini-form-box small-6 medium-6 large-6 columns">
               <div className="mini-contact-wrapper">
-                <form>
-                  <input className="mini-contact-input" type="text" ref="txt_name" placeholder="Your Name"/>
-                  <input className="mini-contact-input" type="email" ref="txt_email" placeholder="Email Address"/>
-                  <select required className="round mini-contact-input no-top-margin " ref="drp_reason" placeholder="Reason for Contacting">
-                    <option value="" disabled selected hidden>Reason for Contacting</option>
-                    <option value="0">Website Design</option>
-                    <option value="1">Sales Suit</option>
-                    <option value="2">Other</option>
+                <form onSubmit={this.onSubmit}>
+                  <input className="mini-contact-input" type="text" ref="name" placeholder="Your Name"
+                    value = {this.state.submitBtnState == 2 ? '' : this.value}
+                  />
+                  <input className="mini-contact-input" type="email" ref="email" placeholder="Email Address"
+                    value = {this.state.submitBtnState == 2 ? '' : this.value}
+                  />
+                  <input className="mini-contact-input" type="text" ref="phone" placeholder="Contact Number"
+                    value = {this.state.submitBtnState == 2 ? '' : this.value}
+                  />
+
+                  <select required onChange={this.handleSelect} className="round mini-contact-input no-top-margin " placeholder="Reason for Contacting" ref="drpPurpose" >
+                    <option value="0"  selected={this.state.optionsState == 0}>Request A Demo</option>
+                    <option value="1" selected={this.state.optionsState == 1}>Sales Inquiry</option>
+                    <option value="2" selected={this.state.optionsState == 2}>Customer Support</option>
+                    <option value="3" selected={this.state.optionsState == 3}>Other</option>
                   </select>
-                  <textarea className="mini-text-area" rows="4" cols="50" ref="txtarea_msg" placeholder="Your lovely feedback goes here..." />
+
+                  <textarea className="mini-text-area" rows="4" cols="50" ref="message" placeholder="Your lovely feedback goes here..."
+                    value = {this.state.submitBtnState == 2 ? '' : this.value}
+                  />
                   <div className="mini-submit-wrapper"
                     onClick={() => {
-                      this.refs.txtarea_msg.focus();
-                    }}> <button type="submit" className="button btn-submit-green-mini" onClick={() => {
-                      alert('contact btn clicked');
-                    }}><big><FaEnvelope/>&nbsp;Send</big></button> </div>
+                    }}> <button type="submit" className="button btn-submit-green-mini" >{renderSubmitButton()}</button> </div>
                 </form>
               </div>
 
