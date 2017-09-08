f2d6b2c2bb1adf0e135e2c5f7b85c846423bbff1
HaswinVidanage <hashdevan@gmail.com>
2017-09-09 02:23:58 +0530

    fix deployment shell script to stay open after execution

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Starting deployment script..."
 echo "Commit label : $1."
-git add -A && git commit -am "$1" && git push -u origin master 
-echo "Completed."
+git add -A && git commit -am "$1" && git push -u origin master
+echo "Deployment completed."
+$SHELL
