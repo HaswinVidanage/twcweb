a4e133251a4082a0422dd06a668eea5aa9a36a06
HaswinVidanage <hashdevan@gmail.com>
2017-09-09 02:18:56 +0530

    fix justify about message  responsive fix for small screens

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting deployment script..."
 echo "Commit label : $1."
-git add -A && git commit -am "$1" && git push -u origin master && git push heroku master
+git add -A && git commit -am "$1" && git push -u origin master 
 echo "Completed."
